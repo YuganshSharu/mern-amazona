720cc068d9b6d89272c43d102a5f14b07ca6f23c
Yugansh Sharu <yugansh19347@iiitd.ac.in>
2022-06-28 12:38:31 +0530

    new build and remove use-reducer-logger from HMS

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useReducer, useState } from 'react';
 // import data from '../data';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -23,7 +22,7 @@ const reducer = (state, action) => {
 };
 
 function HomeScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
